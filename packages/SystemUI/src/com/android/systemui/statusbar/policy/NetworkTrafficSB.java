Myself5/android_frameworks_base-1

base: Network traffic: Retain our notch check
Even with adding the network traffic indicator to the statusbar icon list
and holder, the indicators sometime glitch out with notch devices. With
this check we ensure that nothing gets out of place while still playing
nice with everything else.

Change-Id: If3817d7a95a46c1a9d0555b606dcd1ce19014eff
Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -31,6 +31,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.internal.util.cr.CrUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.plugins.DarkIconDispatcher;
@@ -74,7 +75,6 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     private boolean indicatorUp = false;
     private boolean indicatorDown = false;
     private boolean mHideArrow;
-
     private boolean mScreenOn = true;
 
     private Handler mTrafficHandler = new Handler() {
@@ -397,7 +397,8 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     }
 
     private void updateVisibility() {
-        if (mIsEnabled && mTrafficVisible && mSystemIconVisible) {
+        if (!CrUtils.hasNotch(mContext) && mIsEnabled &&
+                mTrafficVisible && mSystemIconVisible) {
             setVisibility(View.VISIBLE);
         } else {
             setVisibility(View.GONE);

Singhaarav059/android_frameworks_base-3

packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -31,6 +31,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.internal.util.aospa.Utils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.plugins.DarkIconDispatcher;
@@ -74,7 +75,6 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     private boolean indicatorUp = false;
     private boolean indicatorDown = false;
     private boolean mHideArrow;
-
     private boolean mScreenOn = true;
 
     private Handler mTrafficHandler = new Handler() {
@@ -397,7 +397,8 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     }
 
     private void updateVisibility() {
-        if (mIsEnabled && mTrafficVisible && mSystemIconVisible) {
+        if (!Utils.hasNotch(mContext) && mIsEnabled &&
+                mTrafficVisible && mSystemIconVisible) {
             setVisibility(View.VISIBLE);
         } else {
             setVisibility(View.GONE);